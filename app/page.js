048a385f4229773b58764375b927cc85841df8e7
@Lewisjohnward <lewisjohnward+git1@gmail.com>
2023-05-31 16:07:07 +0100

    first commit of working web app

--- app/page.js
+++ app/page.js
@@ -1,95 +1,36 @@
+'use client'
 import Image from 'next/image'
 import styles from './page.module.css'
+import { useState } from 'react'
+import randomColor from 'randomcolor'
+import greetings from './greetings.js'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    const [background, setBackground] = useState(randomColor())
+    const [greeting, setGreeting] = useState(greetings[0])
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+    const handleMouseEnter = () => {
+        setBackground(randomColor({luminosity: 'light'}))
+    }
+    const handleMouseExit = () => {
+        const random = Math.floor(Math.random() * greetings.length)
+        const greeting = greetings[random]
+        console.log(greeting)
+        setGreeting(greeting)
+    }
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    return (
+            <main 
+                className={styles.main}
+                style={{backgroundColor: background}}
+            >
+                <div 
+                    onMouseEnter={() => handleMouseEnter()}
+                    onMouseLeave={() => handleMouseExit()}
+                    className={styles.container}
+                >
+                    {greeting}
+                </div>
+            </main>
+    )
 }
